182ef41d6478e4d092c04bfc13257d64b14f0e5a
vladislav-cherkas <vladislavcherkas2005a@gmail.com>
2025-07-10 23:15:19 +0300

    200 OK

--- script.js
+++ script.js
@@ -5,11 +5,36 @@ function toNarrowVariant() {
         element.style.overflow = 'hidden';
         element.style.width = '0';
     }
+    for (let element of document.querySelectorAll('.row>.day-cell:last-child')) {
+        element.style.flexGrow = '0';
+        element.style.overflow = 'hidden';
+        element.style.width = '0';
+    }
 }
 function toWideVariant() {
     document.querySelector('.restricter').style.width = '840px';
     for (let element of document.querySelectorAll('.row>.executor-cell:last-child')) {
         element.style.flex = '1 1 0';
-        element.style.width = '0';
+    }
+    for (let element of document.querySelectorAll('.row>.day-cell:last-child')) {
+        element.style.flexGrow = '1';
+        element.style.width = 'auto';
+    }
+}
+function pickRandomOption(element) {
+    const options = element.options;
+    if (options.length > 0) {
+        const randomIndex = Math.floor(Math.random() * options.length);
+        element.selectedIndex = randomIndex;
+    }
+}
+function pickRandomOptionAll() {
+    for (let element of document.querySelectorAll('.executor-cell>select')) {
+        pickRandomOption(element);
+    }
+}
+function clearOptionAll() {
+    for (let element of document.querySelectorAll('.executor-cell>select')) {
+        element.selectedIndex = 0;
     }
 }
